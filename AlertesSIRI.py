4124ed1282ab40957af73b4151e7ed2388551e1b
TekMaTe-lux <m.fresse54@gmail.com>
2025-06-12 14:02:52 +0200

    Update AlertesSIRI.py

--- AlertesSIRI.py
+++ AlertesSIRI.py
@@ -43,7 +43,7 @@ def parse_siri_sx(url):
 
 def sauvegarder_json(data, fichier="Assistant-train/siri_sx_alertes.json"):
     os.makedirs("Assistant-train", exist_ok=True)
-    with open("Assistant-train/siri_sx_alertes", "w") as f:
+    with open(fichier, "w") as f:
         json.dump(data, f, ensure_ascii=False, indent=2)
     print(f"{len(data)} situations enregistrées dans {fichier}")
 
