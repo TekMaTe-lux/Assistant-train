6dafde437a584ac5f2168c5f07da09e38d745bc8
TekMaTe-lux <m.fresse54@gmail.com>
2026-08-04 07:50:13 +0200

    Bump traffic modal assets

--- config/territory.nancy-metz-lux.js
+++ config/territory.nancy-metz-lux.js
@@ -60,14 +60,14 @@ window.LB_TERRITORY_CONFIG = Object.freeze({
     const stylesheet = document.createElement('link');
     stylesheet.id = 'lbTrafficDetailsStyles';
     stylesheet.rel = 'stylesheet';
-    stylesheet.href = './assets/lb-traffic-details-v1.css?v=3';
+    stylesheet.href = './assets/lb-traffic-details-v1.css?v=4';
     document.head.append(stylesheet);
   }
 
   if (!document.getElementById('lbTrafficDetailsScript')) {
     const script = document.createElement('script');
     script.id = 'lbTrafficDetailsScript';
-    script.src = './assets/lb-traffic-details-v1.js?v=3';
+    script.src = './assets/lb-traffic-details-v1.js?v=4';
     script.async = false;
     document.head.append(script);
   }
