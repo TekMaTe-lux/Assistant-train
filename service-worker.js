f74e4aa3974844419396faa2f44718e814ece086
TekMaTe-lux <m.fresse54@gmail.com>
2026-08-27 08:58:50 +0200

    Rafraîchit le cache après correction des voies Luxembourg

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = 'v30';
+const CACHE_VERSION = 'v31';
 const APP_CACHE = `lbetaillere-app-${CACHE_VERSION}`;
 const STATIC_CACHE = `lbetaillere-static-${CACHE_VERSION}`;
 const CACHE_PREFIX = 'lbetaillere-';
