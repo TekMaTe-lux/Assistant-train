aea39b0aeff6bac60793d92c2fc367284f0ac198
TekMaTe-lux <m.fresse54@gmail.com>
2026-09-23 10:53:06 +0200

    Charge le nouveau bandeau perturbation majeure

--- assets/home-major-alerts.js
+++ assets/home-major-alerts.js
@@ -13,7 +13,7 @@
   };
 
   // Script d’alertes historique conservé à l’identique.
-  load('./assets/home-major-alerts-core.js?v=20260923-2', 'lb-home-major-alert-core');
+  load('./assets/home-major-alerts-core.js?v=20260923-3', 'lb-home-major-alert-core');
 
   // Pont gare dynamique Luxembourg -> fiche train #BER.
   load('./assets/lux-train-sheet.js?v=20260826-1', 'lb-lux-train-sheet');
