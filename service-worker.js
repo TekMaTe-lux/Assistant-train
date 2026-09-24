7d159e3ea1bb664ce6f96399a7d3dc1cd31d3602
TekMaTe-lux <m.fresse54@gmail.com>
2026-09-24 14:05:33 +0200

    Refresh cache for full CFL station table

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = 'v69';
+const CACHE_VERSION = 'v70';
 const APP_CACHE = `lbetaillere-app-${CACHE_VERSION}`;
 const STATIC_CACHE = `lbetaillere-static-${CACHE_VERSION}`;
 const DATA_CACHE = `lbetaillere-data-${CACHE_VERSION}`;
